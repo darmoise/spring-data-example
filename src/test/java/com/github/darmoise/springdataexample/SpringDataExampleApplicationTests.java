9ee52c943ff35c0ce2aeb1c295fa49486cf03f70
darmoise <darmoise@ubuntu>
2022-09-12 12:09:53 +0300

    first commit

---
+++ src/test/java/com/github/darmoise/springdataexample/SpringDataExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.darmoise.springdataexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
