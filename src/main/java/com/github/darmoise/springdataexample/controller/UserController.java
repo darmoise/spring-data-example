bbbd2bf49b97ce186588f73260caaf582752f502
darmoise <darmoise@ubuntu>
2022-09-16 18:36:15 +0300

    Added AddUserRequestDto

--- src/main/java/com/github/darmoise/springdataexample/controller/UserController.java
+++ src/main/java/com/github/darmoise/springdataexample/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.github.darmoise.springdataexample.controller;
 
+import com.github.darmoise.springdataexample.dto.AddUserRequestDto;
 import com.github.darmoise.springdataexample.dto.UserDto;
 import com.github.darmoise.springdataexample.service.UserService;
 import com.github.darmoise.springdataexample.util.UserMapper;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -38,7 +41,7 @@ public class UserController {
     }
 
     @PostMapping
-    public UserDto addUser(@RequestBody UserDto dto) {
+    public UserDto addUser(@RequestBody AddUserRequestDto dto) {
         val user = userService.addUser(userMapper.dtoToModel(dto));
 
         return userMapper.modelToDto(user);
@@ -47,5 +50,15 @@ public class UserController {
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable UUID userId) {
         userService.deleteUser(userId);
+    }
+
+    @PostMapping("/{userId}/birth-date/{birthDate}")
+    public void changeBirthDate(
+        @PathVariable final UUID userId,
+        @PathVariable
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+        final LocalDate birthDate
+    ) {
+        userService.changeBirthDate(userId, birthDate);
     }
 }
